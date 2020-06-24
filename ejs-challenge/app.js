486dd3181e1bc1d45a3b53e21d88c454b7c870a5
Sabyrzhan Tynybayev <stynybayev@twilio.com>
2020-06-24 03:46:41 +0300

    Finished Chapter 22

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { escapeRegExp } = require("lodash");
+const lodash = require('lodash/string');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,23 +12,53 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/', function(req, res) {
+  return res.render('home', {content: homeStartingContent, title: 'Home', posts: posts});
+});
 
+app.get('/posts/:postName', function(req, res) {
+  let param = lodash.lowerCase(req.params.postName);
+  let postTitle = "N/A";
+  let postContent = "N/A";
+  for(let i = 0; i < posts.length; i++) {
+    if (param == lodash.lowerCase(posts[i].title))  {
+      postTitle = posts[i].title;
+      postContent = posts[i].content;
+    }
+  }
 
+  return res.render('post', {content: postContent, title: postTitle});
+});
 
+app.get('/contact', function(req, res) {
+  return res.render('template', {content: contactContent, title: 'Contact'});
+});
 
+app.get('/about', function(req, res) {
+  return res.render('template', {content: aboutContent, title: 'About'});
+});
 
+app.get('/compose', function(req, res) {
+  return res.render('compose', {title: 'Compose'});
+});
 
+app.post('/compose', function(req, res) {
+  let post = {
+    title: req.body.postTitle,
+    content: req.body.post
+  };
 
+  posts.push(post);
 
-
-
-
-
+  return res.redirect('/compose');
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
